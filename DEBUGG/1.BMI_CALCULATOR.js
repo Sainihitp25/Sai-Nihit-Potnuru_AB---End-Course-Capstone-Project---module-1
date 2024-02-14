05039461ffccde003e2d16d9971f2b13bea96062
sai nihit <sainihitp@gmail.com>
2024-02-14 20:14:42 +0530

    Update 1.BMI_CALCULATOR.js

--- DEBUGG/1.BMI_CALCULATOR.js
+++ DEBUGG/1.BMI_CALCULATOR.js
@@ -1,1 +1,18 @@
-console.log("Hello BMI!!!");
+function calculateBMI(weight, height) {
+    const bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        return "Normal weight";
+    } else if (bmi >= 25 && bmi < 29.9) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+}
+let result = calculateBMI(65, 1.75);
+//let result = calculateBMI(45, 1.60);
+//let result = calculateBMI(104, 1.45);
+
+console.log(result);
