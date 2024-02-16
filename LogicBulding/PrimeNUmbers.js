4207f1243088bcc4b33672d4bd9015d528648ec2
sai nihit <sainihitp@gmail.com>
2024-02-16 17:19:45 +0530

    Create PrimeNUmbers.js

---
+++ LogicBulding/PrimeNUmbers.js
@@ -0,0 +1,18 @@
+function findPrimes(n){
+    let primes = [];
+    for (let i = 2; i <= n; i++) {
+        let isPrime = true;
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push(i);
+        }
+    }
+    return primes;
+}
+
+console.log(findPrimes(10));
