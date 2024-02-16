e6807e953ed608c88ee07fb92dc5a7853f69b78b
sai nihit <sainihitp@gmail.com>
2024-02-16 15:03:19 +0530

    Create CountOccurrences.js

---
+++ Improvised Perspective/CountOccurrences.js
@@ -0,0 +1,9 @@
+
+function countOccurrences(str, char) {
+    const regex = new RegExp(char, 'g');
+    const matches = str.match(regex);
+    return matches ? matches.length : 0;
+}
+console.log(countOccurrences("hello world", "l"))
+console.log(countOccurrences("Elephant", "E"))
+module.exports = { countOccurrences };
